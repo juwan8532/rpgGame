7846ba9838cd04f49de8bd9eded9e40231e6e8d3
juwan <juwanramos@gmail.com>
2020-11-14 23:12:42 -0500

    something

--- createPlayer.js
+++ createPlayer.js
@@ -11,24 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
         const createPassword = document.getElementById('createPassword');
         const recreatePassword = document.getElementById('recreatePassword');
         const badWords = '/-*=+!@#$%^&()"][}{~';
-        const isPassing = false;
+        const isPassing = true;
         //if not valid make text pop up in red
         if(createName.value.length < 7){
             isValidDiv.innerHTML = "User Name needs to be at least 7 characters."
             isValidDiv.style.cssText = 'font-size: 13px; color: red'
+            isPassing = false;
         }
         if(createPassword.value.length < 7){
             const newDiv = isValidDiv.appendChild(div)
             newDiv.innerHTML = "Password needs to be at least 7 characters."
+            isPassing = false;
         }
 
         if(createPassword.value !== recreatePassword.value){
             isValidDiv2.innerHTML = "Passwords do not match."
             isValidDiv2.style.cssText = 'font-size: 13px; color: red'
             isValidDiv2.innerHTML = "Passwords do not match."
+            isPassing = false;
         }
 
-
+        if(isPassing){
+            return true;
+        }
         //--------the bad word check isnt working ----------
 
         // if(userName.value.includes(badWords)){
@@ -43,8 +48,8 @@ document.addEventListener('DOMContentLoaded', () => {
         let result = isValid();
 
         if(result){
-
-        }
+            location.href = "./playerSelection.html"
+        }//if we pass all the requirements to make an account we go to selection page
     })
 
     loginButton.addEventListener('click', (event) =>{
---
+++ playerSelection.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const rogue = document.getElementById('rogue');
+    const knight = document.getElementById('knight');
+    const mage = document.getElementById('mage');
+    const elf = document.getElementById('elf');
+
+    const rogueImg = document.createElement('img');
+    const elfImg = document.createElement('img');
+    const knightImg = document.createElement('img');
+    const mageImg = document.createElement('img');
+
+
+})
