bbb1ffc85f3cc0ff650555d106c0dfb238136e2e
juwan8532 <36888000+juwan8532@users.noreply.github.com>
2023-06-12 14:01:43 -0400

    time to build the world

---
+++ canvas/canvas.js
@@ -0,0 +1,16 @@
+var canvas=document.getElementById('canvas');
+var context=canvas.getContext('2d')
+
+var mapArray = [
+
+  [0,0,0,0,1,1,0,0,0,0,0,0,0],
+  [0,0,0,0,1,1,0,0,0,0,0,0,0],
+  [0,0,0,0,1,1,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,1,1,1,0,0,0,0],
+  [0,0,0,0,0,1,1,1,1,0,0,0,0],
+  [0,0,0,0,0,0,0,0,1,1,0,0,0],
+];
+
+var grass = new Image();
+var sand = new Image();
+
