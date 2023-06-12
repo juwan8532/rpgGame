19fe87910b5869da1b5cd02949289ba3cbb3bc7f
juwan8532 <36888000+juwan8532@users.noreply.github.com>
2023-06-12 14:35:53 -0400

    map array being made

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -14,3 +14,21 @@ var mapArray = [
 var grass = new Image();
 var sand = new Image();
 
+grass.src='/assets/grass.png';
+sand.src='/assets/sand.png';
+
+var posX = 0
+var posY = 0
+
+for(i = 0; i < mapArray.length; i++){
+    for(j = 0; j < mapArray[i].length; j++){
+
+        if(mapArray[i][j] == 0){
+            context.drawImage(grass, posX, posY, 32,32)
+        }
+        if(mapArray[i][j] == 1){
+            context.drawImage(sand, posX, posY, 32,32)
+        }
+    }
+}
+
